0cadfb20e53eee7424664606d12c84d7cba3cc82
sssctkt <justaskaisssctkt@gmail.com>
2026-02-06 12:33:06 -0500

    Add files via upload

---
+++ 01_variables/02 rules on variable names.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //what makes it a good variable name?
+    int two_phrases; //it has to be one phrase
+    int x361; //you cannot start number;
+    //int @#$@#; //cannot use symbols (except _)
+    //int cout{3} ; //do not clash with the keyword
+    
+    //what makes it a good variable name;
+    //1. try to use lower cases 
+    //2. try to be indicative 
+    int radius;
+    //recall, how to declare a variable
+    float diameter, area;
+    //how to initialize a variable
+    float perimeter{34.5};
+    
+    //your turn 
+    //1. I want you to declare 3 different integer variable (in one line)
+    //2. I want you to declare 2 float variable and initialize 1 float variable 
+    //set the value to be 3.4 (in one line)
+    //3. I want you to initialize a string variable, the value is "Finally, it's Friday"
+    return 0;
+}
+
+
+
+
+
+
