535f01dcaab2dd12002c6416cf2f5891744df082
sssctkt <justaskaisssctkt@gmail.com>
2026-02-06 12:32:46 -0500

    Create 01 variable.cpp

---
+++ 01_variables/01 variable.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //variables--to store and manipulate data 
+    //in c++, before you create a variable, you need to understand
+    //"type"
+    //most commonly used type 
+    //int (integer-->no decimal), float (decimal, up to 6 decimal place)
+    //double (decimal, up to 9 decimal places)
+    //char (character)-->one symbol 
+    //string (sentence)
+    //declare 3 integer variables
+    int a, b; //declare a variable
+    int x{0}, y{0}, z{0};//if you declare and give it a value right away
+    //initialization
+    //it might be better to give them initial value
+    cout<<x<<endl; //very careful, if you declare variable and you don't have 
+    //a value assigned, it will start as random value
+    cout<<y<<endl;
+    return 0;
+}
+
+
+
+
+
+
