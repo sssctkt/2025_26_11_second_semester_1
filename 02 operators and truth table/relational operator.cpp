fcd62fe6e6943ceeee7073b5613e3eaf44dbd4ae
sssctkt <justaskaisssctkt@gmail.com>
2026-03-23 08:54:37 -0400

    Add files via upload

---
+++ 02 operators and truth table/relational operator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //review math operator:
+    // % remainder
+    // +, -, *, / 
+    
+    //assignment operator
+    // = += -= *= /= assign value from right to left
+    
+    //new
+    //relational operator (goal: determine true or false) 
+    //true or false can be represented in many ways
+    //integer: 1 mean true, 0 means false
+    //boolean: true or false
+    
+    // > greater than 
+    // < less than 
+    // >= greater than or equal to 
+    // <= less than or equal to
+    // == equal 
+    // != not equal
+    int x;
+    x=(8!=10);
+    cout<<x;
+    
+    return 0;
+}
